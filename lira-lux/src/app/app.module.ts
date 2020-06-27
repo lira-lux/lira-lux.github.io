f9b315ea02810998cca18ddbd4a3964b6b59361d
lira-lux <65965605+lira-lux@users.noreply.github.com>
2020-06-27 16:41:45 +0300

    created contacts component

--- lira-lux/src/app/app.module.ts
+++ lira-lux/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { AboutRotoComponent } from './about-roto/about-roto.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutUsComponent,
-    AboutRotoComponent
+    AboutRotoComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
