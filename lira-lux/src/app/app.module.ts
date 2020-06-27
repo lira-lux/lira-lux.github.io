2a1e5b92d39794175dbb21061d6327252c4048f4
lira-lux <65965605+lira-lux@users.noreply.github.com>
2020-06-27 16:15:00 +0300

    created about us component & about roto component

--- lira-lux/src/app/app.module.ts
+++ lira-lux/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutRotoComponent } from './about-roto/about-roto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutUsComponent,
+    AboutRotoComponent
   ],
   imports: [
     BrowserModule,
