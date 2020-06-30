44f72616b68b280fa93ea154fa0704598701f230
lira-lux <65965605+lira-lux@users.noreply.github.com>
2020-06-30 21:33:46 +0300

    first consultation: create cards and modal conponents

--- lira-lux/src/app/app.module.ts
+++ lira-lux/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { AboutRotoComponent } from './about-roto/about-roto.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardsWindowComponent } from './cards-window/cards-window.component';
+import { CardComponent } from './card/card.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,18 @@ import { FooterComponent } from './footer/footer.component';
     AboutUsComponent,
     AboutRotoComponent,
     ContactsComponent,
-    FooterComponent
+    FooterComponent,
+    CardsWindowComponent,
+    CardComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ModalComponent],
+  entryComponents: [ModalComponent, CardComponent]
 })
 export class AppModule { }
